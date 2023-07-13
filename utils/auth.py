82648fa1a0f936ab695a3035004195936318e8c8
degerahmet <adeger34@gmail.com>
2023-07-12 17:51:29 -0700

    changed files

--- utils/auth.py
+++ utils/auth.py
@@ -8,5 +8,5 @@ def authenticate(jwtToken):
         'id': '1234567890',
         'walletAddress': '0x1234567890'
     }
-    ## TODO: Add logic to cheasdasd
+    ## TODO:kljasdkjl;asd;kj
     return status, user
