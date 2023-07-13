14724f031c29d13b9d02d30a616c85a414fdbd2e
degerahmet <adeger34@gmail.com>
2023-07-12 17:27:40 -0700

    yml updated and auth status updated as True

--- utils/auth.py
+++ utils/auth.py
@@ -3,7 +3,7 @@ def authenticate(jwtToken):
     """
     This function is used to authenticate the user by checking the JWT token
     """
-    status = False
+    status = True
     user = {
         'id': '1234567890',
         'walletAddress': '0x1234567890'
